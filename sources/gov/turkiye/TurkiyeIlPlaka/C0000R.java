44e599d80d57efd2947aed4efe80b248067c56fd
cambazm <56502154+cambazm@users.noreply.github.com>
2019-10-16 19:06:19 +0300

    Add files via upload

---
+++ sources/gov/turkiye/TurkiyeIlPlaka/C0000R.java
@@ -0,0 +1,30 @@
+package gov.turkiye.TurkiyeIlPlaka;
+
+/* renamed from: gov.turkiye.TurkiyeIlPlaka.R */
+public final class C0000R {
+
+    /* renamed from: gov.turkiye.TurkiyeIlPlaka.R$attr */
+    public static final class attr {
+    }
+
+    /* renamed from: gov.turkiye.TurkiyeIlPlaka.R$drawable */
+    public static final class drawable {
+        public static final int icon = 2130837504;
+    }
+
+    /* renamed from: gov.turkiye.TurkiyeIlPlaka.R$id */
+    public static final class C0001id {
+        public static final int search_box = 2131034112;
+    }
+
+    /* renamed from: gov.turkiye.TurkiyeIlPlaka.R$layout */
+    public static final class layout {
+        public static final int main = 2130903040;
+    }
+
+    /* renamed from: gov.turkiye.TurkiyeIlPlaka.R$string */
+    public static final class string {
+        public static final int app_name = 2130968577;
+        public static final int hello = 2130968576;
+    }
+}
